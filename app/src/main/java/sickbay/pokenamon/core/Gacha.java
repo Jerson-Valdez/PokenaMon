c391e4d40f77c4a787175df62149aa18d3a5c350
Jerson <valdezjerson8624@gmail.com>
2026-03-31 18:21:15 +0800

    putting all buttons and image into the init and get its instance from layout

--- app/src/main/java/sickbay/pokenamon/core/Gacha.java
+++ app/src/main/java/sickbay/pokenamon/core/Gacha.java
@@ -1,6 +1,8 @@
 package sickbay.pokenamon.core;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +10,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import sickbay.pokenamon.R;
 
 public class Gacha extends AppCompatActivity {
+    Button draw10x, draw1x;
+    ImageView pokeball;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.layout_gacha);
+        init();
+    }
+
+    private void init(){
+        draw10x = findViewById(R.id.draw10x);
+        draw1x = findViewById(R.id.draw1x);
+        pokeball = findViewById(R.id.pokeball);
+
+
     }
 }
